fb2252edb1678cd6a73ca423f912456c3026c361
Chem++ <julianfrig@outlook.com>
2021-08-09 17:58:56 -0700

    More href enhancemnt using JavaScript
    Co-Authored-By: null <julianfrigcoding@outlook.com>
    Co-Authored-By: Makus_Frig <MakusFrig@users.noreply.github.com>
    Co-Authored-By: Julian <SlipperyDuck777@users.noreply.github.com>
    Co-Authored-By: Chem++ <ChemPlusPlusOffical@users.noreply.github.com>
    Co-Authored-By: null <crthpl@users.noreply.github.com>

--- redirect.js
+++ redirect.js
@@ -13,3 +13,47 @@ function DiscordRedirect() {
 function ConsoleRedirect() {
   location.replace("https://github.com/chemplusplus/Chem_Console")
 }
+
+function SlipperyRedirect() {
+  location.replace("https://github.com/SlipperyDuck777")
+}
+
+function WebContributeRedirect() {
+  location.replace("https://github.com/chemplusplus/chemplusplus.github.io")
+}
+
+function StableRelease() {
+  location.replace("https://github.com/chemplusplus/Chemplusplus/releases/tag/0.2")
+}
+
+function LatestRelease() {
+  location.replace("https://github.com/chemplusplus/Chemplusplus/releases/tag/0.2.1")
+}
+
+function StableReleaseConsole() {
+  location.replace("https://github.com/chemplusplus/Chem_Console/releases/tag/0.1")
+}
+
+function LatestReleaseConsole() {
+  location.replace("https://github.com/chemplusplus/Chem_Console/releases/tag/0.1")
+}
+
+function PubChemPeriodic() {
+  location.replace("https://pubchem.ncbi.nlm.nih.gov/periodic-table/")
+}
+
+function InstallGit() {
+  location.replace("https://www.jcchouinard.com/install-git/")
+}
+
+function PubChemPeriodic() {
+  location.replace("https://pubchem.ncbi.nlm.nih.gov/periodic-table/")
+}
+
+function InstallPy() {
+  location.replace("https://python.org")
+}
+
+function InstallTk() {
+  location.replace("https://docs.python.org/3/library/tkinter.html")
+}
