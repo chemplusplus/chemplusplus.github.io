fd393d6f913653367db887ecd970d3ec6a5381ad
Chem++ <julianfrig@outlook.com>
2021-08-09 17:35:36 -0700

    More effective redirects
    uses JavaScript functions instead of hrefs so it may be easier to see how many times links have been clicked in the future

---
+++ redirect.js
@@ -0,0 +1,15 @@
+function DocumentationRedirect() {
+  location.replace("https://github.com/Chemplusplus/Chemplusplus#documentation")
+}
+
+function GithubRedirect() {
+  location.replace("https://github.com/Chemplusplus/Chemplusplus")
+}
+
+function DiscordRedirect() {
+  location.replace("https://discord.gg/UpYTWFYuHX")
+}
+
+function ConsoleRedirect() {
+  location.replace("https://github.com/chemplusplus/Chem_Console")
+}
